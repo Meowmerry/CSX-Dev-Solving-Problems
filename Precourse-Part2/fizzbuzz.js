f76fe8ecc3fa5a2d710f61ef8c9dbf92ba09fbf3
thasanee <meow3525@gmail.com>
2022-03-19 11:29:29 -0500

    fizzbuzz

--- Precourse-Part2/fizzbuzz.js
+++ Precourse-Part2/fizzbuzz.js
@@ -65,3 +65,27 @@ for (let i = 1; i <= 21; i++) {
     }
 }
 console.log(fizzbuzzList)
+
+/* ========================== SOLUTION 4 ======================================
+================================================================================ */
+const fizzbuzz4 = num => {
+    const result = [];
+    for (let i = 0; i <= num; i += 1) {
+        let str = '';
+        if (i % 3 === 0) str += 'fizz';
+        if (i % 5 === 0) str += 'buzz';
+        result.push(str || i)
+    }
+    return result;
+}
+console.log(fizzbuzz4(16))
+    /*
+[
+'fizzbuzz', 1,      2,
+'fizz',     4,      'buzz',
+'fizz',     7,      8,
+'fizz',     'buzz', 11,
+'fizz',     13,     14,
+'fizzbuzz', 16
+]
+*/
